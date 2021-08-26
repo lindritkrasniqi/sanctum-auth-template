04a17d63d3e1eaffcc47e395c00f799645d35edd
Lindrit Krasniqi <linndritks@gmail.com>
2021-08-26 09:28:22 +0200

    Add a default user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(1)->create([
+            'name' => 'Lindrit Krasniqi',
+            'email' => 'lindritkrasniqi@example.com'
+        ]);
     }
 }
