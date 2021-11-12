21414ae9a69cfc567e131ded569f0ca8c2886a33
Lindrit Krasniqi <linndritks@gmail.com>
2021-11-12 13:05:02 +0100

    Use RefreshDatabase trait, run with seeders and define credentials on TestBase::class

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,18 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+    use CreatesApplication, RefreshDatabase;
+
+    protected $seed = true;
+
+    // credentials for existing User
+    protected $credentials = [
+        "email" => "lindritkrasniqi@example.com",
+        "password" => "password"
+    ];
 }
